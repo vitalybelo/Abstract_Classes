1140e6b97b8d4c6d10e16ee48f9588a259f33599
Vitaly <vitaly.belo@gmail.com>
2023-01-25 11:02:52 +0300

    check_out

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main
 
     public static void main(String[] args) {
 
+        Lamp lamp = new Lamp(100);
+
+        lamp.setBrightness(0.5);
+        lamp.changeBrightness(0.5);
+        System.out.println("Яркость лампы = " + lamp.brightness);
+        System.out.println("Потребление энергии = " + lamp.getEnergyConsumption());
 
     }
 }
